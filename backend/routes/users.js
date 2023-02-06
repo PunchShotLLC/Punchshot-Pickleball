2c3e531cbb330bfb868baec3c56d2df26bafa48d
Logan Weekes <ltweekes1@gmail.com>
2023-02-05 21:30:51 -0500

    first commit

---
+++ backend/routes/users.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+router.route('/:id').get((req, res, next) => {
+
+});
+
+router.post('/add').post((req, res, next) => {
+
+});
+
+router.route('/delete/:id').delete((req, res, next) => {
+
+});
+
+module.exports = router;
