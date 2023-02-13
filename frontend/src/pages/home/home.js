8eb1dd463033d437db5b480a6fe838d55b257889
Logan Weekes <ltweekes1@gmail.com>
2023-02-12 19:41:59 -0500

    add template code to Home

---
+++ frontend/src/pages/home/home.js
@@ -0,0 +1,15 @@
+import { makeStyles }  from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    
+});
+
+export const Home = () => {
+    const classes = useStyles();
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
