c7d27f24fce44da822c362391af89ebdf7b67b9d
Logan Weekes <ltweekes1@gmail.com>
2023-02-12 19:34:51 -0500

    add material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.scss';
+import './App.css';
 
 function App() {
   return (
---
+++ frontend/src/components/header/header.js
@@ -0,0 +1,15 @@
+import { makeStyles }  from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    
+});
+
+export const Header = () => {
+    const classes = useStyles();
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
