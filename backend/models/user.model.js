1d8161d60fa133d8058b2cadde7785928593b56d
reesewang25 <reesewang10@gmail.com>
2023-02-26 15:08:53 -0500

    added user schema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,8 +2,40 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+    Email:{
+        type: String,
+        required: true
+        
+    },
+    Username:{
+        type: String,
+        required: true
+    },
+    Password:{
+        type: String,
+        required: true
+    },
+    FirstName:{
+        type: String,
+    },
+    LastName:{
+        type: String,
+    },
+    ZipCode:{
+        type: Number
+    },
+    SkillLevel:{
+        type: String
+    },
+    Bio:{
+        type: String
+    },
+    ProfilePhoto:{
+        type: String
+    }
+});
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);//looks for "users" collection in mongodb
 
 module.exports = User;
