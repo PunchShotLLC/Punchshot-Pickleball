027bd931615acefafe7d987a55417a8fe12cfe82
reesewang25 <reesewang10@gmail.com>
2023-03-25 15:08:56 -0400

    working get, add, delete requests

--- backend/controllers/tournamentController.js
+++ backend/controllers/tournamentController.js
@@ -1,17 +1,50 @@
-let Tournament = require('.../models/tournament.model');
+let Tournament = require('../models/tournament.model');
 
-const getTournament = async (req, res) => {
-    
+const getTournaments = async (req,res) => {
+    const alltournaments = await Tournament.find({}).sort({createdAt: -1})
+    res.status(200).json(alltournaments)
 }
 
-const addTournament = async(req,res) => {
+const getTournament = async (req, res) => {
+    const tournament = await Tournament.findById(req.params.id)
+        .then((doc) => {
+            res.status(200).json(doc)
+        })
+        .catch((error) => {
+            res.status(400).json({error: error.message});
+        })
+}
 
+const createTournament = async(req,res) => {
+    const {Tournament_Name, Skill_Level, Divisions_Offered, Entry_Fee, Prize, Registration_Deadline} = req.body;
+
+    try {
+        const newTournament = await Tournament.create({Tournament_Name, Skill_Level, Divisions_Offered, Entry_Fee, Prize, Registration_Deadline});
+        //res.json({mssg:"POST a new tournament"})
+        res.status(200).json(newTournament);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
 }
 
 const deleteTournament = async(req, res) => {
-
+    await Tournament.findByIdAndDelete(req.params.id)
+        .then((doc) => {
+            res.status(200).json(doc)
+        })
+        .catch((error) => {
+            res.status(400).json({error: error.message});
+        })
 }
 
+/*
 const updateTournament = async(req,res) => {
 
+}*/
+
+module.exports = {
+    getTournament,
+    getTournaments,
+    createTournament,
+    deleteTournament
 }
--- backend/routes/tournaments.js
+++ backend/routes/tournaments.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 let Tournament = require('../models/tournament.model');
 
 const {
-    getTournament, createTournament, deleteTournament, updateTournament
+    getTournament, createTournament, deleteTournament, getTournaments
 } = require('../controllers/tournamentController')
 
+//all routes on express router
 router.route('/:id').get(getTournament);
+router.route('/').get(getTournaments);
 router.post('/add', createTournament);
 router.route('/delete/:id').delete(deleteTournament);
 
