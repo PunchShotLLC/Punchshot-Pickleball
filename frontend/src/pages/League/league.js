03ba9006144b87fb10567bcc90e43223beabe7e7
Mehdi Bencheqroun <mehdibencheqroun04@gmail.com>
2023-04-11 19:07:32 -0400

    added bottom left feature in league page

--- frontend/src/pages/League/league.js
+++ frontend/src/pages/League/league.js
@@ -9,32 +9,60 @@ import '@fontsource/inter/200.css';
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/700.css';
 import LeagueGrid from "./leagueGrid.js";
+import Grid from '@mui/material/Grid';
+import CustomizedInputs from "../../components/login/CustomInput";
 
 
 
 export const League = () => {
   return (
 
-    <Box sx={{width: '50vw', height: '77.69vh', display: 'flex'}}>
+    <Box sx={{width: '80vw', height: '77.69vh', display: 'flex'}}>
 
-      <Box sx = {{position: "relative", width: '100%', top : "1.5%", height: '100%', display: "flex", flexDirection: "column"}}>
+      <Box sx = {{position: "relative", width: '100%', height: '100%', display: "flex", flexDirection: "column"}}>
 
-        <Typography className="titleText" sx={{ display: "flex", fontSize: 'calc(0.7em + 1vw)', fontWeight: 'bold', pt: '1%'}}>
-          RESULTS
+        <Typography className="titleText" sx={{ display: "flex", fontSize: 'calc(0.7em + 1vw)', fontWeight: 'bold', pt: '1%', align: 'center', marginLeft: '10vw'}}>
+          MY LEAGUE
         </Typography>
-        <Typography className="bodyText" sx={{ display: "absolute", fontSize: 'calc(0.5em + 1vw)', fontWeight: 'bold', pt: '0%', top: '10%'}}>
-          Previous Entries
+        <Typography className="bodyText" sx={{ display: "absolute", fontSize: 'calc(0.5em + 1vw)', fontWeight: 'bold', align: 'center', marginLeft: '10vw'}}>
+          Atlanta Enthusiasts
         </Typography>
-        <Box sx = {{position: "relative", top : '5%', left: '2vw'}}>
+        <Box sx = {{position: "relative", left: '5svw'}}>
+        <LeagueGrid/>
+
+  
+        <Typography className="bodyText" sx={{fontSize: 'calc(0.5em + 0.5vw)', fontWeight: 'bold', align: 'center', marginLeft: '20vw', color: '#9146D8', marginTop: '2vh'}}>
+          League statistics
+        </Typography>
+    
+        <Box sx = {{width: '40vw', height: '4vh', display: 'flex', marginLeft: '6vw'}}>
+
+        <Typography className="bodyText" sx={{fontSize: 'calc(0.5em + 0.5vw)', fontWeight: 'bold', align: 'center', alignitems: 'baseline', display: 'flex', }}>
+          Competitors: 25       
+        </Typography>
+        <Typography className="bodyText" sx={{fontSize: 'calc(0.5em + 0.5vw)', fontWeight: 'bold', align: 'center', alignitems: 'baseline', display: 'flex', marginLeft: '1.5vw'}}>
+        Games Played: 125           
+        </Typography>
+        <Typography className="bodyText" sx={{fontSize: 'calc(0.5em + 0.5vw)', fontWeight: 'bold', align: 'center', alignitems: 'baseline', display: 'flex', marginLeft: '1.5vw'}}>
+        League Participation: 98%
+        </Typography>
+
+        </Box>
           <Box sx = {{position: "absolute", top: '102%', left: '9.5%', width: '100%', height: '100%', display: "flex", flexDirection: 'row'}}>
           
           </Box>
 
-          <LeagueGrid/>
 
         </Box>
       </Box>
 
+      <Box sx={{width: '40vw', height: '77.69vh', display: 'flex', alignItems}}>
+      <Typography className="titleText" sx={{ display: "flex", fontSize: 'calc(0.7em + 1vw)', fontWeight: 'bold', pt: '1%', marginLeft: '9vw', marginBottom: '73vh'}}>
+          CREATE LEAGUE
+        </Typography>
+
+        </Box>
+       
     </Box>
   );
 };
