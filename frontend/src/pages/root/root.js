06c5c7456e11af1ae99518907755d23eda4ea727
Joshua Boisvert <joshmboisvert@gmail.com>
2023-04-12 12:18:48 -0400

    modified root component

--- frontend/src/pages/root/root.js
+++ frontend/src/pages/root/root.js
@@ -1,11 +1,5 @@
 import { Header } from "../../components/header/header";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
-import { requirePropFactory } from "@mui/material";
-import { styled } from "@mui/system";
-// import "./home.css";
 import { Login } from "../../components/login/login.js";
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
