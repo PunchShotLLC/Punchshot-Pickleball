82addae69516ffcbf978f2a0bd30277baa3978f9
Mehdi Bencheqroun <mehdibencheqroun04@gmail.com>
2023-04-16 17:14:13 -0400

    new buttons for league creation and improved UI

--- frontend/src/pages/League/league.js
+++ frontend/src/pages/League/league.js
@@ -2,7 +2,6 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import * as React from 'react';
 import Typography from "@mui/material/Typography";
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight.js'
 import '../League/league.scss'
 import "@fontsource/inter";
 import '@fontsource/inter/200.css';
@@ -12,7 +11,7 @@ import LeagueGrid from "./leagueGrid.js";
 import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/InputBase';
 import { FormControl } from '@mui/material';
-import Select from '@mui/material/Select';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const StyledInput = styled(TextField)({
   borderRadius: '1em',
@@ -22,20 +21,21 @@ const StyledInput = styled(TextField)({
   paddingLeft: "1vw"
 });
 
-const StyledSelect = styled(Select)({
-  borderRadius: '1em',
-  border: '3px solid #000000',
-  fontSize: 'calc(0.8vw + 0.1em)',
-  height: "5h",
-  width: "30vw",
-  paddingLeft: "1vw",
-  bottom: "1em"
-});
-
 const StyledLabel = styled('label')({
   paddingLeft: "1vw",
   marginBottom: "0.5vh",
 });
+
+const buttonTheme = createTheme({
+  palette: {
+      primary: {
+          main: '#9146D8',
+      },
+      secondary: {
+          main: '#D9D9D9',
+      }
+  }
+})
 
 export const League = () => {
   return (
@@ -50,23 +50,23 @@ export const League = () => {
         <Typography className="bodyText" sx={{ display: "absolute", fontSize: 'calc(0.5em + 1vw)', fontWeight: 'bold', align: 'center', marginLeft: '10vw'}}>
           Atlanta Enthusiasts
         </Typography>
-        <Box sx = {{position: "relative", left: '5svw'}}>
+        <Box sx = {{position: "relative", left: '3svw'}}>
         <LeagueGrid/>
 
   
-        <Typography className="bodyText" sx={{fontSize: 'calc(0.5em + 0.5vw)', fontWeight: 'bold', align: 'center', marginLeft: '20vw', color: '#9146D8', marginTop: '2vh'}}>
+        <Typography className="bodyText" sx={{fontSize: 'calc(0.5em + 1.5vw)', fontWeight: 'bold', align: 'center', marginLeft: '15vw', color: '#9146D8', marginTop: '1vh'}}>
           League statistics
         </Typography>
     
-        <Box sx = {{width: '40vw', height: '4vh', display: 'flex', marginLeft: '6vw'}}>
+        <Box sx = {{width: '45vw', height: '4vh', display: 'flex'}}>
 
-        <Typography className="bodyText" sx={{fontSize: 'calc(0.5em + 0.5vw)', fontWeight: 'bold', align: 'center', alignitems: 'baseline', display: 'flex', }}>
+        <Typography className="bodyText" sx={{fontSize: 'calc(0.5em + 0.6vw)', fontWeight: 'bold', align: 'center', alignitems: 'baseline', display: 'flex', marginLeft: '4vw'}}>
           Competitors: 25       
         </Typography>
-        <Typography className="bodyText" sx={{fontSize: 'calc(0.5em + 0.5vw)', fontWeight: 'bold', align: 'center', alignitems: 'baseline', display: 'flex', marginLeft: '1.5vw'}}>
+        <Typography className="bodyText" sx={{fontSize: 'calc(0.5em + 0.6vw)', fontWeight: 'bold', align: 'center', alignitems: 'baseline', display: 'flex', marginLeft: '1.5vw'}}>
         Games Played: 125           
         </Typography>
-        <Typography className="bodyText" sx={{fontSize: 'calc(0.5em + 0.5vw)', fontWeight: 'bold', align: 'center', alignitems: 'baseline', display: 'flex', marginLeft: '1.5vw'}}>
+        <Typography className="bodyText" sx={{fontSize: 'calc(0.5em + 0.6vw)', fontWeight: 'bold', align: 'center', alignitems: 'baseline', display: 'flex', marginLeft: '1.5vw'}}>
         League Participation: 98%
         </Typography>
 
@@ -79,7 +79,7 @@ export const League = () => {
         </Box>
       </Box>
 
-      <Box sx={{width: '40vw', height: '77.69vh', display:"flex", justifyContent:"space-between", alignItems: 'flex-start',
+      <Box sx={{width: '45vw', height: '77.69vh', display:"flex", justifyContent:"space-between", alignItems: 'flex-start',
           flexDirection: 'column'}}>
         <Typography className="titleText" sx={{ display: "flex", fontSize: 'calc(0.7em + 1vw)', fontWeight: 'bold', pt: '1%'}}>
           CREATE LEAGUE
@@ -87,7 +87,7 @@ export const League = () => {
         <Box sx={{height:"70vh", width:'40vw', marginLeft:'5vw', borderLeft:'2px solid rgba(145, 70, 216, 1)'}} >
                     <FormControl sx={{height:"5vw", marginLeft:'1.5vw'}}>
                         <StyledLabel htmlFor="leagueName">League Name <span style={{color:"red"}}>*</span></StyledLabel>
-                        <StyledInput id="leagueName" placeholder="Atlanta Leagueg" required />
+                        <StyledInput id="leagueName" placeholder="Atlanta Classic League" required />
                     </FormControl>
                     <FormControl sx={{height:"5vw", marginLeft:'1.5vw'}}>
                         <StyledLabel htmlFor="nbCompetitors">Number of Competitors <span style={{color:"red"}}>*</span></StyledLabel>
@@ -113,9 +113,14 @@ export const League = () => {
                         <StyledLabel htmlFor="email">Email <span style={{color:"red"}}>*</span></StyledLabel>
                         <StyledInput id="email" placeholder="email@example.com" required />
                     </FormControl>
+                    <ThemeProvider theme={buttonTheme}>
+                    <div className='login_button_grid'>
+                        <Button variant='contained' color='secondary' sx={{ position: 'relative', borderRadius: 'calc(0.1em + 0.5vw)', pl: 'calc(1.5vw)', pr: 'calc(1.8vw)'}}>Clear Inputs</Button>
+                        <Button variant='contained' color='primary' sx={{ position: 'relative', borderRadius: 'calc(0.1em + 0.5vw)', pl: 'calc(1.5vw)', pr: 'calc(1.8vw)'}}>Create League</Button>
+                    </div>
+                    </ThemeProvider>
           </Box>
       </Box>
-       
     </Box>
   );
 };
