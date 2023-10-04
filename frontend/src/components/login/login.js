f9b327b0c5bc81b294ed409bb1e39c7a720a4435
Logan Weekes <ltweekes1@gmail.com>
2023-10-04 17:08:05 -0400

    fix styling on login popup

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -99,27 +99,29 @@ export const Login = (props) => {
     if (props.render == true) {
         return (
             <ThemeProvider theme={buttonTheme}>
-                <Box sx={{ width: '31.25vw', height: '31.25vw', background: 'white', borderRadius: 'calc(0.1em + 1vw)', position: 'absolute', left: '34.4vw', top: '22.22vh', zIndex: '5'}}>
-                    <img className="login_x_button" src={x_button} onClick={() => props.setRender((oldRender) => !oldRender)}></img>
+                <Box sx={{ width: '30vw', height: '30vw', background: 'white', borderRadius: 'calc(0.1em + 1vw)', position: 'absolute', left: '34.4vw', top: '22.22vh', zIndex: '5', display:'flex', flexDirection: 'column', alignItems: 'center'}}>
+                    <Box sx={{width: '30vw', height: '2vw', display: 'flex', flexDirection: 'column', alignItems:'flex-end'}}>
+                        <img className="login_x_button" src={x_button} onClick={() => props.setRender((oldRender) => !oldRender)}></img>
+                    </Box>
                     <img className="login_logo_image" src={logo}></img>
-                    <Typography align="center" sx={{ position: 'relative', mt: "10%" }}>Log into your account, or sign up here!</Typography>
+                    <Typography align="center" sx={{ position: 'relative'}}>Log into your account, or sign up here!</Typography>
                     <div className="login_input">
                         <CustomInput 
-                                text={Username}                               
+                                text={'Username'}                              
                                 varChange={(e) => setUsername(e.target.value)}
                                 />
                     </div>
                     <div className="login_input">
                         <CustomInput 
-                                text={Password}
+                                text={'Password'}
                                 varChange={(e) => setPassword(e.target.value)}
                                 />
                     </div>
-                    <div className='login_button_grid'>
-                        <Button onClick={handleCreate} variant='contained' color='secondary' sx={{ position: 'relative', borderRadius: 'calc(0.1em + 0.5vw)', pl: 'calc(1.8vw)', pr: 'calc(1.8vw)' }}>Create Account</Button>
-                        <Button onClick={handleLogin} variant='contained' color='primary' sx={{ position: 'relative', borderRadius: 'calc(0.1em + 0.5vw)', pl: 'calc(4vw)', pr: 'calc(4vw)' }}>Sign In</Button>
-                    </div>
-                    <Link color='primary' sx={{position:'relative', top:'3%', left: '37%'}}>Forgot Password?</Link>
+                    <Box sx={{width:'25vw', display:'flex', flexDirection:'row', justifyContent:'space-between', marginTop:'1vh'}}>
+                        <Button onClick={handleCreate} variant='contained' color='secondary' sx={{ position: 'relative', borderRadius: 'calc(0.1em + 0.5vw)', pl: 'calc(1.8vw)', pr: 'calc(1.8vw)', width:'11vw', height:'2vw', fontSize:'calc(0.8vh + 0.29em)'}}>Create Account</Button>
+                        <Button onClick={handleLogin} variant='contained' color='primary' sx={{ position: 'relative', borderRadius: 'calc(0.1em + 0.5vw)', pl: 'calc(4vw)', pr: 'calc(4vw)', width:'11vw', height:'2vw', fontSize:'calc(0.8vh + 0.29em)' }}>Sign In</Button>
+                    </Box>
+                    <Link color='primary' sx={{marginTop: '2vh' }}>Forgot Password?</Link>
                 </Box>
                 <Box sx={{zIndex:'3', minWidth:'100vw', minHeight:'100vh', backgroundColor:'lightgray', opacity:'0.8', position: 'absolute'}}></Box>
             </ThemeProvider>
