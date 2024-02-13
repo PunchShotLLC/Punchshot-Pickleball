646a4f40bc37a1a0b082a9760e5f62ab38cbad4a
Li, Congyan <Zoey_07@163.com>
2024-02-13 00:41:13 -0500

    Update LeagueComp.js
    Add a "View Team" button and a disclaimer modal. Add a "More Info" button and an info modal.

--- frontend/src/components/LeagueComp/LeagueComp.js
+++ frontend/src/components/LeagueComp/LeagueComp.js
@@ -5,6 +5,9 @@ import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { borderRadius } from "@mui/system";
 import { Button } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Modal } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 export const LeagueComp = (props) => {
   function isBeforeToday(dateStr) {
@@ -177,11 +180,164 @@ export const LeagueComp = (props) => {
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
+  const buttonTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#9146D8",
+      },
+      secondary: {
+        main: "#D9D9D9",
+      },
+    },
+  });
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalInfoOpen, setModalInfoOpen] = useState(false);
+
+  const openDisclaimerModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeDisclaimerModal = () => {
+    setModalOpen(false);
+  };
+
+  const handleUnderstand = () => {
+    props.onClick(); // Navigate to "/leagueInfo" page
+  };
+
+  const openInfoModal = () => {
+    setModalInfoOpen(true);
+  };
+
+  const closeInfoModal = () => {
+    setModalInfoOpen(false);
+  };
+
   if (props.showLeague) {
     return (
       <Box sx={styles.main}>
-        <Box onClick={props.onClick} sx={styles.side}>
-          <Typography sx={styles.name}>{props.name}</Typography>
+        <Box sx={styles.side}>
+          {/* Disclaimer Modal */}
+          <Modal open={modalOpen}>
+            <Box sx={styles.modal}>
+              <Typography>
+                If you wish the join the league, make sure to have at least 4
+                members in your team before the team registration date. Each
+                member will then be charged $10.00 to be paid before the start
+                date.
+              </Typography>
+              <ThemeProvider theme={buttonTheme}>
+                <Box sx={styles.row}>
+                  <Button
+                    onClick={closeDisclaimerModal}
+                    sx={{
+                      ...styles.button,
+                      width: "30%",
+                      transition: "background-color 0.3s",
+                      "&:hover": {
+                        backgroundColor: buttonTheme.palette.primary.main,
+                      },
+                    }}
+                    variant="contained"
+                    color="secondary"
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    onClick={handleUnderstand}
+                    sx={{
+                      ...styles.button,
+                      width: "50%",
+                      transition: "background-color 0.3s",
+                      "&:hover": {
+                        backgroundColor: buttonTheme.palette.primary.main,
+                      },
+                    }}
+                    variant="contained"
+                    color="secondary"
+                  >
+                    I understand
+                  </Button>
+                </Box>
+              </ThemeProvider>
+            </Box>
+          </Modal>
+
+          {/* League Info Modal*/}
+          <Modal open={modalInfoOpen}>
+            <Box sx={styles.modal}>
+              <Box sx={styles.column}>
+                <Box sx={{ ...styles.data, ...styles.modalData }}>
+                  <img
+                    src={require("../../assets/images/Team.png")}
+                    alt="team"
+                    width="4%"
+                    style={{ border: "2px solid black", borderRadius: "50%" }}
+                  />
+                  <Typography sx={styles.modalData}>
+                    Number of Teams: {props.teamsSignedUp}/{props.numberOfTeams}{" "}
+                    Teams
+                  </Typography>
+                </Box>
+                <Box sx={{ ...styles.data, ...styles.modalData }}>
+                  <img
+                    alt="clock"
+                    src={require("../../assets/images/Clock.png")}
+                    width="5%"
+                    style={{ borderRadius: "50%" }}
+                  />
+                  <Typography sx={styles.modalData}>
+                    Team Registration Date:{" "}
+                    {new Date(props.registrationDate).toLocaleDateString()}
+                  </Typography>
+                </Box>
+                <Box sx={{ ...styles.data, ...styles.modalData }}>
+                  <img
+                    alt="clock"
+                    src={require("../../assets/images/Clock.png")}
+                    width="5%"
+                    style={{ borderRadius: "50%" }}
+                  />
+                  <Typography sx={styles.modalData}>
+                    Start Date: {new Date(props.startDate).toLocaleDateString()}
+                  </Typography>
+                </Box>
+                <Button
+                  onClick={closeInfoModal}
+                  variant="contained"
+                  sx={{ ...styles.button }}
+                  color="secondary"
+                >
+                  Back
+                </Button>
+              </Box>
+            </Box>
+          </Modal>
+
+          {/* View Team and More Info Button */}
+          <Box sx={styles.row}>
+            <Typography sx={styles.name}>{props.name}</Typography>
+            <ThemeProvider theme={buttonTheme}>
+              <Button
+                onClick={openDisclaimerModal}
+                variant="contained"
+                color="primary"
+                sx={styles.button}
+              >
+                View Teams
+              </Button>
+              <Button
+                onClick={openInfoModal}
+                variant="contained"
+                color="primary"
+                sx={styles.button}
+              >
+                More Info
+              </Button>
+            </ThemeProvider>
+          </Box>
+
           <Box sx={styles.row}>
             <Box sx={styles.data}>
               <img
@@ -263,6 +419,10 @@ const styles = {
     alignItems: "center",
     width: LeagueComp.isSmallScreen ? "25%" : "16.67%",
   },
+  modalData: {
+    gap: "2%",
+    width: "80%",
+  },
   main: {
     backgroundColor: "#F5F5F5",
     borderRadius: "10px",
@@ -277,6 +437,13 @@ const styles = {
   row: {
     display: "flex",
     flexDirection: "row",
+    alignItems: "center",
+  },
+  column: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: "2vh",
   },
   info: {
     color: "black",
@@ -287,5 +454,26 @@ const styles = {
     color: "black",
     width: "20vh",
     fontSize: "0.75em",
+  },
+  button: {
+    borderRadius: "calc(1.5em + 1vw)",
+    marginLeft: "2em",
+    width: "20%",
+  },
+  modal: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "35%",
+    height: "auto",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#F5F5F5",
+    borderRadius: "10px",
+    padding: "4vh",
+    gap: "2vh",
   },
 };
